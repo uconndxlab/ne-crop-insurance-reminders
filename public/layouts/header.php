98014c0104df96d981ee703d354313ef55e23e3e
Joel Salisbury <joel.salisbury@gmail.com>
2024-01-18 11:47:38 -0500

    meta viewport

--- public/layouts/header.php
+++ public/layouts/header.php
@@ -2,6 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="USDA RMA and FSA Programs App">
+    <!-- set the viewport to the width of the device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>USDA RMA and FSA Programs App</title>
     <!-- bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
